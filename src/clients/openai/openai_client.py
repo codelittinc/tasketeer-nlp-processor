b0622e9b55feb5d814612e941591e423b091e1ab
Alessandro <alessandromontividiu03@gmail.com>
2023-03-15 18:21:11 -0300

    Allow to append new files to the latest LLM index

--- src/clients/openai/openai_client.py
+++ src/clients/openai/openai_client.py
@@ -1,28 +1,37 @@
 from llama_index import (
-    GPTTreeIndex, 
+    GPTTreeIndex,
     LLMPredictor,
     download_loader,
 )
 import os
 from langchain import OpenAI
 
-def generate_string_index(content):
+def _generate_llm_predictor():
   temperature = int(os.environ.get('OPENAI_PREDICTOR_TEMPERATURE', '0'))
   model_name = os.environ.get('OPENAI_PREDICTOR_MODEL_NAME', 'text-davinci-003')
   
+  # define LLM with OpenAI model name and temperature
+  return LLMPredictor(llm=OpenAI(temperature=temperature, model_name=model_name))
+
+def generate_string_index(content, gpt_latest_index_str):
   # download the string loader, in case it doesn't exists
   StringIterableReader = download_loader("StringIterableReader")
   loader = StringIterableReader()
   
   # set the content to be indexed
   documents = loader.load_data(texts=[content])
+  
+  # define LLM with OpenAI model name and temperature
+  llm_predictor = _generate_llm_predictor()
 
-  # define LLM
-  llm_predictor = LLMPredictor(llm=OpenAI(temperature=temperature, model_name=model_name))
-
-  # build index
-  index = GPTTreeIndex(documents, llm_predictor=llm_predictor)
-
+  # in case there is a previous index, add the new documents to it
+  if gpt_latest_index_str is not None:
+    index = GPTTreeIndex.load_from_string(gpt_latest_index_str)
+    index.insert(documents[0], llm_predictor=llm_predictor)
+  else:
+    index = GPTTreeIndex(documents, llm_predictor=llm_predictor)
+  
+  # returns the index as a string
   return index.save_to_string()
 
 
@@ -30,6 +39,18 @@ def search(input, gpt_index_str):
   search_mode = os.environ.get('OPENAI_MODE', 'summarize')
   not_found_answer = os.environ.get('OPENAI_NOT_FOUND_RESPONSE', '')
 
-  # build index
-  index = GPTTreeIndex.load_from_string(gpt_index_str)
-  return index.query(f'{input}.{not_found_answer}', search_mode)
+  if gpt_index_str is None:
+    # download the string loader, in case it doesn't exists
+    StringIterableReader = download_loader("StringIterableReader")
+    loader = StringIterableReader()
+    
+    # set the content to be indexed
+    documents = loader.load_data(texts=['_'])
+    
+    # define LLM with OpenAI model name and temperature
+    llm_predictor = _generate_llm_predictor()
+    index = GPTTreeIndex(documents, llm_predictor=llm_predictor)
+    return index.query(input, search_mode)
+  else:
+    index = GPTTreeIndex.load_from_string(gpt_index_str)
+    return index.query(f'{input}.{not_found_answer}', search_mode)
