bc9a34dbbd3515e3e58bfe17abc649593795edbd
Alessandro <alessandromontividiu03@gmail.com>
2023-03-28 14:05:06 -0300

    Remove search history and fix search if there are no indexed files

--- src/services/search_by_context_service.py
+++ src/services/search_by_context_service.py
@@ -1,24 +1,12 @@
 from src.repositories.file_indexes_repository import *
-from src.repositories.search_history_repository import *
 from src.clients.openai import openai_client
 
 class SearchByContextService():
     def apply(self, data):
       file_index_repository = FileIndexesRepository()
-      search_history_repository = SearchHistoryRepository()
       
       # get the mongodb entity from the organization
       entity = file_index_repository.get_by_organization(data["organization"])
       
-      # send question to open ai
-      response = openai_client.search(data["q"], entity)
-      
-      # add response to the search history collection
-      search_history_repository.insert({
-        'organization': data["organization"],
-        'q': data["q"],
-        'gpt_answer': str(response)
-      })
-      
-      # returns the anwser from open ai
-      return response
+      # send question to open ai and return the answer
+      return openai_client.search(data["q"], entity)
