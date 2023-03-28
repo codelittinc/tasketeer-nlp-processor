a918147c69cf22fc40703249cc005bfc9974b15d
Alessandro <alessandromontividiu03@gmail.com>
2023-03-28 14:05:06 -0300

    Remove search history and fix search if there are no indexed files

--- src/clients/openai/openai_client.py
+++ src/clients/openai/openai_client.py
@@ -4,16 +4,13 @@ from llama_index import (
     download_loader,
 )
 import os
+import openai
 from langchain import OpenAI
 
-def _generate_llm_predictor():
-  temperature = int(os.environ.get('OPENAI_PREDICTOR_TEMPERATURE', '0'))
-  model_name = os.environ.get('OPENAI_PREDICTOR_MODEL_NAME', 'text-davinci-003')
-  
-  # define LLM with OpenAI model name and temperature
-  return LLMPredictor(llm=OpenAI(temperature=temperature, model_name=model_name))
-
 def generate_string_index(content):
+  temperature = int(os.environ.get('OPENAI_PREDICTOR_TEMPERATURE', '0'))
+  model_name = os.environ.get('OPENAI_PREDICTOR_MODEL_NAME', 'gpt-3.5-turbo')
+
   # download the string loader, in case it doesn't exists
   StringIterableReader = download_loader("StringIterableReader")
   loader = StringIterableReader()
@@ -22,7 +19,7 @@ def generate_string_index(content):
   documents = loader.load_data(texts=[content])
   
   # define LLM with OpenAI model name and temperature
-  llm_predictor = _generate_llm_predictor()
+  llm_predictor = LLMPredictor(llm=OpenAI(temperature=temperature, model_name=model_name))
 
   # index the document 
   index = GPTTreeIndex(documents, llm_predictor=llm_predictor)
@@ -34,18 +31,12 @@ def search(input, gpt_index_str):
   search_mode = os.environ.get('OPENAI_MODE', 'summarize')
   not_found_answer = os.environ.get('OPENAI_NOT_FOUND_RESPONSE', '')
 
+  # in case there are no indexed documents, use the default openai completion api
+  # otherwise, use the indexed documents to search for the answer
+  # PS: Completion API does not support the gpt-3.5-turbo model at the time of this writing 
   if gpt_index_str is None:
-    # download the string loader, in case it doesn't exists
-    StringIterableReader = download_loader("StringIterableReader")
-    loader = StringIterableReader()
-    
-    # set the content to be indexed
-    documents = loader.load_data(texts=['_'])
-    
-    # define LLM with OpenAI model name and temperature
-    llm_predictor = _generate_llm_predictor()
-    index = GPTTreeIndex(documents, llm_predictor=llm_predictor)
-    return index.query(input, search_mode)
+    completion = openai.Completion.create(model="text-davinci-003", prompt=f'{input}.{not_found_answer}')
+    return completion.choices[0].text
   else:
     index = GPTTreeIndex.load_from_string(gpt_index_str)
     return index.query(f'{input}.{not_found_answer}', search_mode)
