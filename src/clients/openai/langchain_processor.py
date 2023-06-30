0171bcdfc42bb640ed5f1df6f92c9130cdc06300
Alessandro <alessandromontividiu03@gmail.com>
2023-06-30 14:06:53 -0300

    Fix nlp indexer to store google drive file chunks

--- src/clients/openai/langchain_processor.py
+++ src/clients/openai/langchain_processor.py
@@ -23,6 +23,7 @@ def generate_string_index(content, organization, google_drive_id, google_token, 
     # Split documents into smaller chunks
     text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=20)
     docs = text_splitter.split_documents(documents)
+    google_drive_chunks = text_splitter.split_documents(google_drive_documents)
 
     # Generate embeddings and index
     __init_pinecone()
@@ -33,13 +34,12 @@ def generate_string_index(content, organization, google_drive_id, google_token, 
     )
     
     # Generate embeddings and index for google drive documents
-    if len(google_drive_documents) > 0:
+    if len(google_drive_chunks) > 0:
         Pinecone.from_texts(
-            [t.page_content for t in google_drive_documents], embeddings,
+            [t.page_content for t in google_drive_chunks], embeddings,
             index_name=os.environ.get('PINECONE_INDEX'),
-            namespace=f"organization-google-drive-{organization}"
+            namespace=f"org-google-drive-{organization}"
         )
-    
     return True
 
 
@@ -54,8 +54,8 @@ def search(req_input, organization, openai_api_key, history=None):
     )
     google_drive_chain = Pinecone.from_existing_index(
         os.environ.get('PINECONE_INDEX'), embeddings,
-        namespace=f"organization-google-drive-{organization}"
-    )
+        namespace=f"org-google-drive-{organization}"
+    )    
     
     similar_docs = chain.similarity_search(req_input)
     similar_docs.extend(google_drive_chain.similarity_search(req_input))
@@ -63,7 +63,7 @@ def search(req_input, organization, openai_api_key, history=None):
     prompt = PromptTemplate(template=LANGCHAIN_TEMPLATE, input_variables=["context", "question", "chat_history"])
 
     llm = ChatOpenAI(
-        model_name=os.environ.get('OPENAI_PREDICTOR_MODEL_NAME', 'gpt-3.5-turbo'),
+        model_name=os.environ.get('OPENAI_PREDICTOR_MODEL_NAME', 'gpt-3.5-turbo-16k'),
         temperature=float(os.environ.get('OPENAI_PREDICTOR_TEMPERATURE', '0')),
         max_tokens=int(os.environ.get('OPENAI_PREDICTOR_TOKENS', '3000')),
     )
